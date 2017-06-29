42a24f048a04ab7438a76d8badeb109fc5635ed8
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-06-29 16:53:25 +0700

    complete scenorio loop.

--- src/main/kotlin/loop/loop.kt
+++ src/main/kotlin/loop/loop.kt
@@ -17,4 +17,35 @@ fun main(args: Array<String>) {
         println(index)
         index--
     }
+
+    for (c in "Jedsada") {
+        if (c == 's') break
+        print(c)
+    }
+
+    val listTest = listOf("Book", "Table", "Laptop")
+    for (str in listTest) {
+        if (!str.contains('o')) {
+            continue
+        }
+        println(str)
+    }
+
+    listTest.asSequence()
+            .filter { it.contains('o') }
+            .forEach { println(it) }
+
+    listTest.filter { it.contains('o') }
+            .forEach { println(it) }
+
+    outer@ for (i in 1..10) {
+        for (j in 1..10) {
+            if (i - j == 5) break@outer
+            println("$i - $j")
+        }
+    }
+
+    //challenge
+    val result = (100..100000).sum()
+    println(result)
 }
